9d64681a864871c4b1a8b3e4376d0b8bd860a4a5
Akshay Vadher <akshay@incubyte.co>
2025-03-09 16:39:05 +0530

    WIP

---
+++ web/prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: 'all',
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+};
+
+module.exports = config;
--- web/src/app/sync/useTransactionQueue.ts
+++ web/src/app/sync/useTransactionQueue.ts
@@ -1,44 +1,49 @@
-import {useQueueConsumer} from "@/sync/queue";
+import { useQueueConsumer } from '@/sync/queue';
 import {
   useAddKeyResult,
   useAddObjective,
-  useUpdateKeyResultProgress
-} from "@/sync/object-pool";
-import {usePgLocal} from "@/sync/usePgLocal";
-import {useCallback, useEffect} from "react";
+  useUpdateKeyResultProgress,
+} from '@/sync/object-pool';
+import { usePgLocal } from '@/sync/usePgLocal';
+import { useCallback, useEffect } from 'react';
 import {
   CreateKeyResultRequestWithObjective,
   CreateObjectiveRequest,
   KeyResult,
-  Objective, UpdateProgressRequestWithKeyResult
-} from "@/types";
-import {TransactionEnriched} from "@/sync/transaction";
-import {api} from "@/lib/api";
+  Objective,
+  UpdateProgressRequestWithKeyResult,
+} from '@/types';
+import { TransactionEnriched } from '@/sync/transaction';
+import { api } from '@/lib/api';
 
 let isProcessing = false;
 
 export const useTransactionQueue = () => {
-  const {pendingItems, markAsProcessing, complete, fail} = useQueueConsumer();
+  const { pendingItems, markAsProcessing, complete, fail } = useQueueConsumer();
   const addObjectiveLocal = useAddObjective();
   const addKeyResultLocal = useAddKeyResult();
   const updateKeyResultProgressLocal = useUpdateKeyResultProgress();
 
-  const {db, doesTransactionExist} = usePgLocal();
+  const { db, doesTransactionExist } = usePgLocal();
 
-  const addObjectivePgLocal = useCallback((objective: Objective) => {
-    if (!db) throw new Error("db connection not available");
-    db.exec(`
+  const addObjectivePgLocal = useCallback(
+    (objective: Objective) => {
+      if (!db) throw new Error('db connection not available');
+      db.exec(`
         INSERT INTO objectives (id, title, created_at, updated_at)
         values ('${objective.id}',
                 '${objective.title}',
                 '${objective.created_at}',
                 '${objective.updated_at}')
-    `)
-  }, [db]);
+    `);
+    },
+    [db],
+  );
 
-  const addKeyResultPgLocal = useCallback((keyResult: KeyResult) => {
-    if (!db) throw new Error("db connection not available");
-    db.exec(`
+  const addKeyResultPgLocal = useCallback(
+    (keyResult: KeyResult) => {
+      if (!db) throw new Error('db connection not available');
+      db.exec(`
         INSERT INTO key_results (id, title, target, metrics, objective_id,
                                  created_at, updated_at, current)
         values ('${keyResult.id}',
@@ -50,170 +55,200 @@ export const useTransactionQueue = () => {
                 '${keyResult.updated_at}',
                 '${keyResult.current}')
     `);
-  }, [db])
+    },
+    [db],
+  );
 
-  const updateKeyResultProgressPgLocal = useCallback(({id, progress}: {
-    id: string,
-    progress: number
-  }) => {
-    if (!db) throw new Error("db connection not available");
-    db.exec(`
+  const updateKeyResultProgressPgLocal = useCallback(
+    ({ id, progress }: { id: string; progress: number }) => {
+      if (!db) throw new Error('db connection not available');
+      db.exec(`
         UPDATE key_results
         SET current = ${progress}
         WHERE id = '${id}'
     `);
-  }, [db]);
+    },
+    [db],
+  );
 
-  const transactionLocalInMemoryProcessor = useCallback(async (transaction: TransactionEnriched) => {
-    switch (transaction.entity) {
-      case "OBJECTIVE":
-        switch (transaction.action) {
-          case "CREATE":
-            const request = transaction.payload as CreateObjectiveRequest;
-            addObjectiveLocal({
-              ...request,
-              id: transaction.id,
-              created_at: transaction.created_at,
-              updated_at: transaction.created_at,
-              key_results: [],
-            });
-            break;
-          default:
-            throw new Error(`Unknown action: ${transaction.action}`);
-        }
-        break;
-      case "KEY_RESULT":
-        switch (transaction.action) {
-          case "CREATE": {
-            const request = transaction.payload as CreateKeyResultRequestWithObjective;
-            addKeyResultLocal({
-              ...request,
-              id: transaction.id,
-              created_at: transaction.created_at,
-              updated_at: transaction.created_at,
-              current: 0,
-            })
-            break;
+  const transactionLocalInMemoryProcessor = useCallback(
+    async (transaction: TransactionEnriched) => {
+      switch (transaction.entity) {
+        case 'OBJECTIVE':
+          switch (transaction.action) {
+            case 'CREATE':
+              const request = transaction.payload as CreateObjectiveRequest;
+              addObjectiveLocal({
+                ...request,
+                id: transaction.id,
+                created_at: transaction.created_at,
+                updated_at: transaction.created_at,
+                key_results: [],
+              });
+              break;
+            default:
+              throw new Error(`Unknown action: ${transaction.action}`);
           }
-          case "UPDATE_PROGRESS": {
-            const request = transaction.payload as UpdateProgressRequestWithKeyResult;
-            updateKeyResultProgressLocal({
-              id: request.keyResultId,
-              progress: request.progress,
-            })
-            break;
+          break;
+        case 'KEY_RESULT':
+          switch (transaction.action) {
+            case 'CREATE': {
+              const request =
+                transaction.payload as CreateKeyResultRequestWithObjective;
+              addKeyResultLocal({
+                ...request,
+                id: transaction.id,
+                created_at: transaction.created_at,
+                updated_at: transaction.created_at,
+                current: 0,
+              });
+              break;
+            }
+            case 'UPDATE_PROGRESS': {
+              const request =
+                transaction.payload as UpdateProgressRequestWithKeyResult;
+              updateKeyResultProgressLocal({
+                id: request.keyResultId,
+                progress: request.progress,
+              });
+              break;
+            }
+            default:
+              throw new Error(`Unknown action: ${transaction.action}`);
           }
-          default:
-            throw new Error(`Unknown action: ${transaction.action}`);
-        }
-        break;
-      default:
-        throw new Error(`Unknown entity: ${transaction.entity}`);
-    }
-  }, [addKeyResultLocal, addObjectiveLocal, updateKeyResultProgressLocal])
+          break;
+        default:
+          throw new Error(`Unknown entity: ${transaction.entity}`);
+      }
+    },
+    [addKeyResultLocal, addObjectiveLocal, updateKeyResultProgressLocal],
+  );
 
-  const transactionLocalDbProcessor = useCallback(async (transaction: TransactionEnriched) => {
-    switch (transaction.entity) {
-      case "OBJECTIVE":
-        switch (transaction.action) {
-          case "CREATE":
-            const request = transaction.payload as CreateObjectiveRequest;
-            addObjectivePgLocal({
-              ...request,
-              id: transaction.id,
-              created_at: transaction.created_at,
-              updated_at: transaction.created_at,
-              key_results: [],
-            });
-            break;
-          default:
-            throw new Error(`Unknown action: ${transaction.action}`);
-        }
-        break;
-      case "KEY_RESULT":
-        switch (transaction.action) {
-          case "CREATE": {
-            const request = transaction.payload as CreateKeyResultRequestWithObjective;
-            addKeyResultPgLocal({
-              ...request,
-              id: transaction.id,
-              created_at: transaction.created_at,
-              updated_at: transaction.created_at,
-              current: 0,
-            })
-            break;
+  const transactionLocalDbProcessor = useCallback(
+    async (transaction: TransactionEnriched) => {
+      switch (transaction.entity) {
+        case 'OBJECTIVE':
+          switch (transaction.action) {
+            case 'CREATE':
+              const request = transaction.payload as CreateObjectiveRequest;
+              addObjectivePgLocal({
+                ...request,
+                id: transaction.id,
+                created_at: transaction.created_at,
+                updated_at: transaction.created_at,
+                key_results: [],
+              });
+              break;
+            default:
+              throw new Error(`Unknown action: ${transaction.action}`);
           }
-          case "UPDATE_PROGRESS": {
-            const request = transaction.payload as UpdateProgressRequestWithKeyResult;
-            updateKeyResultProgressPgLocal({
-              id: request.keyResultId,
-              progress: request.progress,
-            })
-            break;
+          break;
+        case 'KEY_RESULT':
+          switch (transaction.action) {
+            case 'CREATE': {
+              const request =
+                transaction.payload as CreateKeyResultRequestWithObjective;
+              addKeyResultPgLocal({
+                ...request,
+                id: transaction.id,
+                created_at: transaction.created_at,
+                updated_at: transaction.created_at,
+                current: 0,
+              });
+              break;
+            }
+            case 'UPDATE_PROGRESS': {
+              const request =
+                transaction.payload as UpdateProgressRequestWithKeyResult;
+              updateKeyResultProgressPgLocal({
+                id: request.keyResultId,
+                progress: request.progress,
+              });
+              break;
+            }
+            default:
+              throw new Error(`Unknown action: ${transaction.action}`);
           }
-          default:
-            throw new Error(`Unknown action: ${transaction.action}`);
-        }
-        break;
-      default:
-        throw new Error(`Unknown entity: ${transaction.entity}`);
-    }
-  }, [addKeyResultPgLocal, addObjectivePgLocal, updateKeyResultProgressPgLocal])
+          break;
+        default:
+          throw new Error(`Unknown entity: ${transaction.entity}`);
+      }
+    },
+    [addKeyResultPgLocal, addObjectivePgLocal, updateKeyResultProgressPgLocal],
+  );
 
-  const transactionApi = useCallback(async (transaction: TransactionEnriched) => {
-    api.addTransaction(transaction).then();
-  }, [])
+  const transactionApi = useCallback(
+    async (transaction: TransactionEnriched) => {
+      api.addTransaction(transaction).then();
+    },
+    [],
+  );
 
-  const processInMemory = useCallback(async (
-      id: string,
-      transaction: TransactionEnriched
-  ) => {
+  const processInMemory = useCallback(
+    async (id: string, transaction: TransactionEnriched) => {
+      try {
+        const { exists } = await doesTransactionExist(transaction.id);
+        if (exists) return;
 
-    try {
-      const {exists} = await doesTransactionExist(transaction.id)
+        console.log('transaction does not exist so processing', transaction);
+        await transactionLocalInMemoryProcessor(transaction);
+        await transactionLocalDbProcessor(transaction);
+      } catch (error) {
+        console.error('process in memory failed', error);
+      }
+    },
+    [
+      doesTransactionExist,
+      transactionLocalDbProcessor,
+      transactionLocalInMemoryProcessor,
+    ],
+  );
+
+  const transactionLocalDb = useCallback(
+    async (transaction: TransactionEnriched) => {
+      const { exists } = await doesTransactionExist(transaction.id);
       if (exists) return;
-
-      console.log('transaction does not exist so processing', transaction)
-      await transactionLocalInMemoryProcessor(transaction)
-      await transactionLocalDbProcessor(transaction)
-    } catch (error) {
-      console.error('process in memory failed', error)
-    }
-  }, [doesTransactionExist, transactionLocalDbProcessor, transactionLocalInMemoryProcessor])
-
-
-  const transactionLocalDb = useCallback(async (transaction: TransactionEnriched) => {
-    const {exists} = await doesTransactionExist(transaction.id)
-    if (exists) return;
-    if (!db) throw new Error("db connection not available");
-    db.exec(`
+      if (!db) throw new Error('db connection not available');
+      db.exec(
+        `
         INSERT INTO transactions (id, entity, action, payload, created_at)
         values ('${transaction.id}',
                 '${transaction.entity}',
                 '${transaction.action}',
                 '${JSON.stringify(transaction.payload)}',
                 '${transaction.created_at}')
-    `).then()
-  }, [db, doesTransactionExist]);
+    `,
+      ).then();
+    },
+    [db, doesTransactionExist],
+  );
 
-  const processItem = useCallback(async (
-      id: string,
-      transaction: TransactionEnriched
-  ) => {
-    markAsProcessing(id);
+  const processItem = useCallback(
+    async (id: string, transaction: TransactionEnriched) => {
+      markAsProcessing(id);
 
-    try {
-      await transactionApi(transaction)
-      await transactionLocalDb(transaction)
-      await transactionLocalInMemoryProcessor(transaction)
-      await transactionLocalDbProcessor(transaction)
+      try {
+        await transactionApi(transaction);
+        await transactionLocalDb(transaction);
+        await transactionLocalInMemoryProcessor(transaction);
+        await transactionLocalDbProcessor(transaction);
 
-      complete(id);
-    } catch (error) {
-      console.error('process failed', error)
-      fail(id, "" + error);
-    }
-  }, [complete, fail, markAsProcessing, transactionApi, transactionLocalDb, transactionLocalDbProcessor, transactionLocalInMemoryProcessor]);
+        complete(id);
+      } catch (error) {
+        console.error('process failed', error);
+        fail(id, '' + error);
+      }
+    },
+    [
+      complete,
+      fail,
+      markAsProcessing,
+      transactionApi,
+      transactionLocalDb,
+      transactionLocalDbProcessor,
+      transactionLocalInMemoryProcessor,
+    ],
+  );
 
   const processNext = useCallback(async () => {
     if (pendingItems.length > 0 && !isProcessing) {
@@ -236,5 +271,5 @@ export const useTransactionQueue = () => {
     };
   }, [pendingItems, processNext]);
 
-  return {processInMemory}
-}
+  return { processInMemory };
+};
